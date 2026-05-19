8a02df30149ac3c9c6f3a3665437ddff38b09003
PardhuERP <sunny.pardhu@gmail.com>
2026-05-19 14:29:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const API="https://script.google.com/macros/s/AKfycbypgdv1t-xNbeykrN6GFdAMz-N8e5hSh4fgZ7W93X-mOn5f1yljanPwHPFzBBE38A/exec";
 
+
 async function materials(){
 
 let r=await fetch(
@@ -18,7 +19,6 @@ x.material+
 x.balance+
 "\nStatus: "+
 x.status+
-
 "\n\n";
 
 });
@@ -27,6 +27,7 @@ alert(html);
 
 }
 
+
 function dailyReport(){
 
 window.open(
@@ -34,6 +35,7 @@ API+"?action=pdfReport"
 );
 
 }
+
 
 async function whatsapp(){
 
@@ -48,6 +50,7 @@ alert(txt);
 
 }
 
+
 async function openRooms(){
 
 document
@@ -56,136 +59,8 @@ document
 
 }
 
+
 async function calculate(){
-
-let unit=
-document.getElementById("unit").value;
-
-let length=parseFloat(
-document.getElementById("length").value
-);
-
-let width=parseFloat(
-document.getElementById("width").value
-);
-
-if(unit=="mm"){
-
-length=length/304.8;
-width=width/304.8;
-
-}
-
-let r=await fetch(
-
-API+
-"?action=calculate"+
-"&length="+length+
-"&width="+width+
-"&height=10"
-
-);
-
-let d=await r.json();
-
-document.getElementById("result").innerHTML=`
-
-<h3>📐 Results</h3>
-
-<b>Area:</b> ${Number(d.area).toFixed(2)} sqft
-
-<br><br>
-
-<b>Tiles:</b> ${d.tiles}
-
-<br><br>
-
-<b>Skirting:</b> ${d.skirting} sqft
-
-<br><br>
-
-<b>Skirting Tiles:</b>
-${Math.ceil((d.skirting/4)+1)}
-
-<br><br>
-
-<b>Total Tiles:</b>
-${d.tiles + Math.ceil((d.skirting/4)+1)}
-
-<br><br>
-
-<b>Adhesive:</b>
-${d.adhesive} Bags
-
-<br><br>
-
-<b>Grout:</b>
-${d.grout}
-
-<br><br>
-
-<b>False Ceiling:</b>
-${d.falseCeiling} sqft
-
-<br><br>
-
-<b>Gypsum Boards:</b>
-${d.gypsum}
-
-<br><br>
-
-<b>Paint:</b>
-${d.paint} Liters
-
-<br><br>
-
-<b>POP Bags:</b>
-${d.pop}
-
-`;
-
-}
-
-document.getElementById("result").innerHTML=`
-
-<div class="card">
-
-<h3>📐 Results</h3>
-
-<p><b>Area:</b> ${Number(d.area).toFixed(2)} sqft</p>
-
-<p><b>Tiles:</b> ${d.tiles}</p>
-
-<p><b>Skirting:</b> ${d.skirting} sqft</p>
-
-<p><b>Skirting Tiles:</b> ${Math.ceil((d.skirting/4)+1)}</p>
-
-<p><b>Total Tiles:</b> ${d.tiles + Math.ceil((d.skirting/4)+1)}</p>
-
-<p><b>Adhesive:</b> ${d.adhesive} Bags</p>
-
-<p><b>Grout:</b> ${d.grout}</p>
-
-<hr>
-
-<p><b>False Ceiling:</b> ${d.falseCeiling} sqft</p>
-
-<p><b>Gypsum Boards:</b> ${d.gypsum}</p>
-
-<p><b>Paint:</b> ${d.paint} Liters</p>
-
-<p><b>POP Bags:</b> ${d.pop}</p>
-
-</div>
-
-`;
-
-async function saveRoom(){
-
-let room=
-document.getElementById(
-"room"
-).value;
 
 let unit=
 document.getElementById(
@@ -206,6 +81,121 @@ document.getElementById(
 
 if(unit=="mm"){
 
+length=length/304.8;
+
+width=width/304.8;
+
+}
+
+let r=await fetch(
+
+API+
+
+"?action=calculate"+
+
+"&length="+length+
+
+"&width="+width+
+
+"&height=10"
+
+);
+
+let d=await r.json();
+
+document.getElementById(
+"result"
+).innerHTML=`
+
+<div class="card">
+
+<h3>📐 Results</h3>
+
+<p><b>Area:</b>
+${Number(d.area).toFixed(2)}
+sqft</p>
+
+<p><b>Tiles:</b>
+${d.tiles}</p>
+
+<p><b>Skirting:</b>
+${d.skirting}
+sqft</p>
+
+<p><b>Skirting Tiles:</b>
+${Math.ceil((d.skirting/4)+1)}</p>
+
+<p><b>Total Tiles:</b>
+${d.tiles+
+Math.ceil((d.skirting/4)+1)}</p>
+
+<p><b>Adhesive:</b>
+${d.adhesive}
+Bags</p>
+
+<p><b>Grout:</b>
+${d.grout}</p>
+
+<hr>
+
+<p><b>False Ceiling:</b>
+${d.falseCeiling}
+sqft</p>
+
+<p><b>Gypsum Boards:</b>
+${d.gypsum}</p>
+
+<p><b>Paint:</b>
+${d.paint}
+Liters</p>
+
+<p><b>POP Bags:</b>
+${d.pop}</p>
+
+</div>
+
+`;
+
+}
+
+
+
+async function saveRoom(){
+
+let room=
+document.getElementById(
+"room"
+).value;
+
+
+let roomType=
+document.getElementById(
+"roomType"
+).value;
+
+
+let unit=
+document.getElementById(
+"unit"
+).value;
+
+
+let length=parseFloat(
+document.getElementById(
+"length"
+).value
+);
+
+
+let width=parseFloat(
+document.getElementById(
+"width"
+).value
+);
+
+
+if(unit=="mm"){
+
 length=
 length/304.8;
 
@@ -214,20 +204,6 @@ width/304.8;
 
 }
 
-let url=
-
-API+
-
-"?action=addRoom"+
-
-"&project=PRJ001"+
-
-"&room="+room+
-
-let roomType=
-document.getElementById(
-"roomType"
-).value;
 
 let url=
 
@@ -247,11 +223,6 @@ API+
 
 "&height=10";
 
-"&length="+length+
-
-"&width="+width+
-
-"&height=10";
 
 let r=
 await fetch(url);
